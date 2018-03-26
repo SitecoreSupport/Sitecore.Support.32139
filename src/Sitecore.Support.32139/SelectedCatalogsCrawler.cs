74c399ed873778dd77b6acd22a7217a348ffbd75
Vu Nhat Tan <onchua2006@gmail.com>
2018-03-26 19:00:52 +0800

    Modify SelectedCatalogsCrawler class to fix NullReferenceException exception in CatalogCrawler.GetIndexable method.

---
+++ src/Sitecore.Support.32139/SelectedCatalogsCrawler.cs
@@ -0,0 +1,27 @@
+﻿
+using Sitecore.Commerce.Connect.CommerceServer.Search.Models;
+using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Support.Commerce.Connect.CommerceServer.Search
+{
+    public class SelectedCatalogsCrawler : Sitecore.Commerce.Connect.CommerceServer.Search.SelectedCatalogsCrawler
+    {
+        protected override CommerceIndexableItem GetIndexable(IIndexableUniqueId indexableUniqueId)
+        {
+            using (new SecurityDisabler())
+            {
+                ItemUri itemUri = indexableUniqueId as SitecoreItemUniqueId;
+                if (itemUri != null)
+                {
+                    return new CommerceIndexableItem(Sitecore.Data.Database.GetItem(itemUri));
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
